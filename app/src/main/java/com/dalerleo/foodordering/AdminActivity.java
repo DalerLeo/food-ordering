fa603cbe4860b5d2df2e98708b168b09aadb6d91
dalerleo <d.abdullaev@student.inha.uz>
2018-12-22 20:41:36 +0500

    New Notify

--- app/src/main/java/com/dalerleo/foodordering/AdminActivity.java
+++ app/src/main/java/com/dalerleo/foodordering/AdminActivity.java
@@ -1,9 +1,11 @@
 package com.dalerleo.foodordering;
 
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.NotificationCompat;
@@ -39,13 +41,52 @@ public class AdminActivity extends AppCompatActivity {
   }
 
   public void AddFood(View view) {
-    NotificationCompat.Builder mBuilder =
-      new NotificationCompat.Builder(this)
-        .setSmallIcon(R.drawable.fav_icon)
-        .setContentTitle("My notification")
-        .setContentText("Hello World!");
+
+    int NOTIFICATION_ID = 234;
+
+    NotificationManager notificationManager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+
+    String CHANNEL_ID = "my_channel_01";
+
+    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+
+
+      CharSequence name = "my_channel";
+      String Description = "This is my channel";
+      int importance = NotificationManager.IMPORTANCE_HIGH;
+      NotificationChannel mChannel = new NotificationChannel(CHANNEL_ID, name, importance);
+      mChannel.setDescription(Description);
+      mChannel.enableLights(true);
+      mChannel.setLightColor(Color.RED);
+      mChannel.enableVibration(true);
+      mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+      mChannel.setShowBadge(false);
+      notificationManager.createNotificationChannel(mChannel);
+    }
+
+    NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+      .setSmallIcon(R.mipmap.ic_launcher)
+      .setContentTitle("HELLO")
+      .setContentText("WORLD");
+
+    Intent resultIntent = new Intent(this, MainActivity.class);
+    TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+    stackBuilder.addParentStack(MainActivity.class);
+    stackBuilder.addNextIntent(resultIntent);
+    PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+    builder.setContentIntent(resultPendingIntent);
+
+    notificationManager.notify(NOTIFICATION_ID, builder.build());
+
+/*
+//    NotificationCompat.Builder mBuilder =
+//      new NotificationCompat.Builder(this)
+//        .setSmallIcon(R.drawable.fav_icon)
+//        .setContentTitle("My notification")
+//        .setContentText("Hello World!");
 // Creates an explicit intent for an Activity in your app
-    Intent resultIntent = new Intent(this, FoodCreate.class);
+//    Intent resultIntent = new Intent(this, FoodCreate.class);
 
 // The stack builder object will contain an artificial back stack for the
 // started Activity.
@@ -67,7 +108,7 @@ public class AdminActivity extends AppCompatActivity {
 // mId allows you to update the notification later on.
     mNotificationManager.notify(123, mBuilder.build());
 
-//    startActivity(new Intent(this, FoodCreate.class));
+//    startActivity(new Intent(this, FoodCreate.class));*/
   }
 
   public void signOut(View view) {
