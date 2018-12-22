c15570f7205b5a0154c1768454c4b3135c7c5ccc
dalerleo <d.abdullaev@student.inha.uz>
2018-12-22 21:24:26 +0500

    Notify done

--- app/src/main/java/com/dalerleo/foodordering/AdminActivity.java
+++ app/src/main/java/com/dalerleo/foodordering/AdminActivity.java
@@ -111,37 +111,7 @@ public class AdminActivity extends AppCompatActivity {
   }
 
   public void AddFood(View view) {
-
-/*
-//    NotificationCompat.Builder mBuilder =
-//      new NotificationCompat.Builder(this)
-//        .setSmallIcon(R.drawable.fav_icon)
-//        .setContentTitle("My notification")
-//        .setContentText("Hello World!");
-// Creates an explicit intent for an Activity in your app
-//    Intent resultIntent = new Intent(this, FoodCreate.class);
-
-// The stack builder object will contain an artificial back stack for the
-// started Activity.
-// This ensures that navigating backward from the Activity leads out of
-// your application to the Home screen.
-    TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
-// Adds the back stack for the Intent (but not the Intent itself)
-    stackBuilder.addParentStack(FoodCreate.class);
-// Adds the Intent that starts the Activity to the top of the stack
-    stackBuilder.addNextIntent(resultIntent);
-    PendingIntent resultPendingIntent =
-      stackBuilder.getPendingIntent(
-        0,
-        PendingIntent.FLAG_UPDATE_CURRENT
-      );
-    mBuilder.setContentIntent(resultPendingIntent);
-    NotificationManager mNotificationManager =
-      (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-// mId allows you to update the notification later on.
-    mNotificationManager.notify(123, mBuilder.build());
-
-//    startActivity(new Intent(this, FoodCreate.class));*/
+    startActivity(new Intent(this, FoodCreate.class));
   }
 
   public void signOut(View view) {
